239f4a3f5c5b33df89256e9f1a571c80d8de8f19
Matthew Frazier <leafstormrush@gmail.com>
2014-04-05 16:22:29 -0400

    split out platforms even more
    Now, there is a node for each station/line/direction combo, with
    turnarounds at termini specifically modeled as edges.
    
    This actually improves efficiency even more since there are less choices
    to consider at each edge, and since the "don't transfer twice in a row"
    can be better enforced.

--- challenge.py
+++ challenge.py
@@ -17,6 +17,10 @@ from unidecode import unidecode
 ### File Loading
 ###
 
+Line = namedtuple('Line', ['name', 'frequency', 'color', 'stations', 'segments'])
+Segment = namedtuple('Segment', ['lines', 'left', 'right', 'time'])
+
+
 def read_system(fd):
     """
     Reads a system description file in a way that it can be converted to
@@ -27,9 +31,7 @@ def read_system(fd):
     names), and `line_segments` (a mapping of line names to lists of
     (station, station) pairs).
     """
-    line_freqs = {}
-    station_lines = {}
-    line_segments = {}
+    lines = {}
 
     for line in fd:
         parts = line.strip().split()
@@ -37,16 +39,23 @@ def read_system(fd):
             continue
 
         if parts[1] == 'Frequency':
+            # Line declaration
             line, _freq_, freq = parts
-            line_freqs[line] = int(freq)
-            line_segments.setdefault(line, set())
+            lines[line] = Line(line, int(freq), line.lower(), [], [])
         else:
+            # Line initialization
             line, stn1, stn2, time = parts
-            line_segments[line].add((stn1, stn2, int(time)))
-            station_lines.setdefault(stn1, set()).add(line)
-            station_lines.setdefault(stn2, set()).add(line)
+            stations, segments = lines[line].stations, lines[line].segments
 
-    return line_freqs, station_lines, line_segments
+            if not stations:
+                stations.append(stn1)
+            elif stations[-1] != stn1:
+                raise ValueError("Stations must be listed sequentially")
+
+            stations.append(stn2)
+            segments.append(Segment(line, stn1, stn2, int(time)))
+
+    return lines
 
 
 ###
@@ -63,21 +72,98 @@ def ascii(french_name):
 ### Digraph Model
 ###
 
-#: A node is a (line, station) pair -- it represents a platform at a station.
+#: A node is a (line, station, direction) pair -- it represents
+#: a platform at a station.
 #: This allows both track segments and transfers to be represented as edges.
-Node = namedtuple('Node', ['line', 'station'])
+Node = namedtuple('Node', ['line', 'station', 'direction'])
 
 
 #: Represents riding a train across a section of track.
 RIDE = "ride"
 
+#: Represents switching direction at the end of the line.
+TURNAROUND = "turnaround"
+
 #: Represents transferring from one train to another at a transfer station.
 TRANSFER = "transfer"
 
 
-def build_digraph(line_freqs, station_lines, line_segments):
+def build_digraph(lines):
     graph = nx.DiGraph()
 
+    transfer_from = {}
+    transfer_to = {}
+
+    # Construct the lines!
+    # We do each line separately.
+    for line in lines.values():
+        # Find the directions!
+        head_station, tail_station = line.stations[0], line.stations[-1]
+
+        # Build the first station!
+        prev_node_head = Node(line.name, head_station, head_station)
+        prev_node_tail = Node(line.name, head_station, tail_station)
+        prev_station = head_station
+
+        # (Also add it to the transfer_from list.)
+        transfer_from.setdefault(head_station, set()).add((line.name, head_station))
+        transfer_to.setdefault(head_station, set()).add((line.name, tail_station))
+
+        graph.add_node(prev_node_head)
+        graph.add_node(prev_node_tail)
+
+        # Iterate through and build the subsequent stations and lines!
+        for n in range(len(line.stations) - 1):
+            new_station = line.stations[n + 1]
+            segment = line.segments[n]
+
+            # Build the station nodes!
+            new_node_head = Node(line.name, new_station, head_station)
+            new_node_tail = Node(line.name, new_station, tail_station)
+
+            # Connect prev_station and next_station!
+            graph.add_edge(prev_node_tail, new_node_tail,
+                           action=RIDE, weight=segment.time, target=segment)
+            graph.add_edge(new_node_head, prev_node_head,
+                           action=RIDE, weight=segment.time, target=segment)
+
+            # Add it to the transfer lists!
+            transfer_from.setdefault(new_station, set()).add((line.name, tail_station))
+            transfer_to.setdefault(new_station, set()).add((line.name, head_station))
+
+            # Add it to the transfer lists, in the other direction!
+            if new_station != tail_station:
+                transfer_from[new_station].add((line.name, head_station))
+                transfer_to[new_station].add((line.name, tail_station))
+
+            prev_node_head, prev_node_tail = new_node_head, new_node_tail
+
+        # Build the turnarounds!
+        # One connects the tail station from tail-direction to head.
+        # One connects the head station from head-direction to tail.
+        graph.add_edge(Node(line.name, tail_station, tail_station),
+                       Node(line.name, tail_station, head_station),
+                       action=TURNAROUND, weight=line.frequency)
+        graph.add_edge(Node(line.name, head_station, head_station),
+                       Node(line.name, head_station, tail_station),
+                       action=TURNAROUND, weight=line.frequency)
+
+    # Now, build the transfers!
+    # (We can iterate over either transfer_from or transfer_to,
+    # since entries are always created in pairs.)
+    for station in transfer_from.keys():
+        transfers = itertools.product(transfer_from[station], transfer_to[station])
+        for (from_line, from_dir), (to_line, to_dir) in transfers:
+            if from_line == to_line:
+                continue
+
+            graph.add_edge(Node(from_line, station, from_dir),
+                           Node(to_line, station, to_dir),
+                           action=TRANSFER, weight=lines[to_line].frequency)
+
+    return graph
+
+    ####
     for station, lines in station_lines.items():
         # Build the stations
         for line in lines:
@@ -106,7 +192,9 @@ def build_digraph(line_freqs, station_lines, line_segments):
             graph.add_edge(Node(line, stn2), Node(line, stn1),
                            weight=time, action=RIDE, target=target)
 
-    return graph
+
+def node_identifier(node):
+    return ascii(node.line) + "_" + ascii(node.station) + "_" + ascii(node.direction)
 
 
 def write_digraph_dot(graph):
@@ -114,25 +202,32 @@ def write_digraph_dot(graph):
     lines.append("digraph metro {")
 
     for node in graph.nodes_iter():
-        name = node.station + "\\n" + node.line
-        ident = ascii(node.line) + "_" + ascii(node.station)
-        lines.append("  {} [label=\"{}\"]".format(ident, name))
+        lines.append("  {} [label=\"{}\", color={}]".format(
+            node_identifier(node),
+            node.station + "\\n" + node.line + "\\nto " + node.direction,
+            node.line.lower()
+        ))
 
     for node1, node2, data in graph.edges_iter(data=True):
         time = data['weight']
-        ident1 = ascii(node1.line) + "_" + ascii(node1.station)
-        ident2 = ascii(node2.line) + "_" + ascii(node2.station)
 
         if data['action'] == TRANSFER:
             color = 'black'
             label = "Wait {}m".format(time)
+            style = "solid"
+        elif data['action'] == TURNAROUND:
+            color = node1.line.lower()
+            label = "Wait {}m".format(time)
+            style = "dashed"
         else:
             color = node1.line.lower()
             label = "{}m".format(time)
+            style = "solid"
 
-        lines.append("  {} -> {} [".format(ident1, ident2))
-        lines.append("    len={}, minlen={},".format(time, time))
-        lines.append("    label=\"{}\", color={}".format(label, color))
+        lines.append("  {} -> {} [".format(node_identifier(node1),
+                                           node_identifier(node2)))
+        #lines.append("    len={}, minlen={},".format(time, time))
+        lines.append("    label=\"{}\", color={}, style={}".format(label, color, style))
         lines.append("  ]")
 
     lines.append("}")
@@ -170,9 +265,9 @@ class PathFinder:
         )
 
         # Precalculate edge ordering
-        # Put RIDE edges before TRANSFER edges, then sort by weight
+        # Put RIDE/TURNAROUND edges before TRANSFER edges, then sort by weight
         self.outbound_edges = {}
-        ranks = {RIDE: 0, TRANSFER: 1}
+        ranks = {RIDE: 0, TURNAROUND: 0, TRANSFER: 1}
         edge_key = lambda e: (ranks[e[2]['action']], e[2]['weight'], e[0], e[1])
 
         for node in graph:
@@ -191,12 +286,14 @@ class PathFinder:
         # Create some starting positions
         empty_path = Path((), frozenset(), 0, self.targets)
         for node in sorted(self.graph):
-            self.queue.append((empty_path, node))
+            self.queue.appendleft((empty_path, node))
 
         # Recursively investigate
-        while len(self.queue) > 0:
+        count = 0
+        while len(self.queue) > 0 and count < 10000000000000:
             path, at_node = self.queue.popleft()
             self.investigate(path, at_node)
+            count = count + 1
 
     def investigate(self, path, at_node):
         options = self.outbound_edges[at_node]
@@ -214,10 +311,10 @@ class PathFinder:
             if edge[:2] in path.used_edges:
                 continue
 
-            # Don't turn around unless it's our only option.
-            if path.edges and len(options) > 1:
+            # Don't transfer if we just transferred.
+            if path.edges:
                 prev_edge = path.edges[-1]
-                if prev_edge[0] == edge[1] and prev_edge[1] == edge[0]:
+                if prev_edge[2]['action'] == TRANSFER and edge[2]['action'] == TRANSFER:
                     continue
 
             # Try it!
@@ -229,7 +326,7 @@ class PathFinder:
                 self.found(new_path)
             else:
                 # Keep following the path!
-                self.queue.append((new_path, new_node))
+                self.queue.appendleft((new_path, new_node))
 
     def found(self, path):
         if self.minimum_time is None or path.time < self.minimum_time:
@@ -249,9 +346,9 @@ class PathFinder:
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as fd:
-        freqs, stns, segments = read_system(fd)
+        lines = read_system(fd)
 
-    digraph = build_digraph(freqs, stns, segments)
+    digraph = build_digraph(lines)
 
     command = sys.argv[2] if len(sys.argv) > 2 else None
 
@@ -264,13 +361,20 @@ if __name__ == '__main__':
         for path in finder.minimum_paths:
             print("In {} minutes:".format(path.time))
             for edge in path.edges:
+                time = edge[2]['weight']
+                print("[{:>3}m] ".format(time), end='')
+
                 if edge[2]['action'] == RIDE:
-                    print("Ride {} from {} to {} [{}m]".format(
-                        edge[0].line, edge[0].station, edge[1].station, edge[2]['weight']
+                    print("Ride from      {:<18} to {:<18} on {:<10} (direction {})".format(
+                        edge[0].station, edge[1].station, edge[0].line, edge[0].direction
+                    ))
+                elif edge[2]['action'] == TURNAROUND:
+                    print("Turn around at {:<18} {:<21} on {:<10} (direction {})".format(
+                        edge[1].station, '', edge[1].line, edge[1].direction
                     ))
                 else:
-                    print("Transfer from {} to {} at {} [{}m]".format(
-                        edge[0].line, edge[1].line, edge[0].station, edge[2]['weight']
+                    print("Transfer at    {:<18} {:<21} to {:<10} (direction {})".format(
+                        edge[0].station, '', edge[1].line, edge[1].direction
                     ))
             print()
 
